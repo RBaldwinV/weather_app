d074a09abe35f7759fccb47ffcd8fd9b59ced2a7
RBaldwinV <rbaldwinv@broken-m.co.uk>
2015-10-20 20:25:48 -0400

    APP + ANIMATION
    Hi Kim!

--- js/app.js
+++ js/app.js
@@ -1,3 +1,13 @@
 // Foundation JavaScript
 // Documentation can be found at: http://foundation.zurb.com/docs
 $(document).foundation();
+
+//var cloud = new Snap('#cloud');
+//var cloudPath = cloud.select('#cloudBorder');
+//cloudPath.animate({opacity:0}, 500);
+//
+//var rain = new Snap('#rain');
+//var rainPath = drop.select('#rainBorder');
+//
+//var night = new Snap('#night');
+//var nightPath = moon.select('#nightBorder');
